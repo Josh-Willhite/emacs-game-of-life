94db33dfc4c8d19d95ea895966a56f960148d468
Josh Willhite <jwillhite@gmail.com>
2021-05-25 20:27:14 -0700

    clean up interface

--- gol.el
+++ gol.el
@@ -1,25 +1,23 @@
 ;; create a new buffer with randomly initialized gol board
-(defconst size-of-board 48)
+(defconst size-of-board 64)
 (defconst board-array (make-vector size-of-board nil))
+(defconst alive (char-from-name "BLACK LARGE SQUARE"))
+(defconst dead (char-from-name "WHITE LARGE SQUARE"))
 
-(defun initialize-square () (if (= 1 (random 2))
-                                (char-from-name "BLACK LARGE SQUARE")
-                              (char-from-name "WHITE LARGE SQUARE")
-                              )
-       )
+(defun gol-initialize-square () (if (= 1 (random 2)) alive dead))
 
-(defun initialize-board ()
+(defun gol-initialize-board ()
   (dotimes (i size-of-board)
     (setf (aref board-array i) (make-vector size-of-board 0))
     (dotimes (j size-of-board)
-      (setf (elt (elt board-array i) j) (initialize-square))
+      (setf (elt (elt board-array i) j) (gol-initialize-square))
       (insert (elt (elt board-array i) j))
       )
     (insert "\n")
     )
   )
 
-(defun get-neighbors (column row)
+(defun gol-get-neighbors (column row)
   (let ((ops `(
                [column (- row 1)]
                [(- column 1) (- row 1)]
@@ -43,15 +41,14 @@
           )
         )
       )
-
     neighbors
     )
   )
 
-(defun neighbor-count (column row)
-  (let ((neighbors (get-neighbors column row)) (count 0))
+(defun gol-neighbor-count (column row)
+  (let ((neighbors (gol-get-neighbors column row)) (count 0))
     (dolist (n neighbors)
-      (if (eq (elt (elt board-array (car (cdr n))) (car n)) (char-from-name "BLACK LARGE SQUARE"))
+      (if (eq (elt (elt board-array (car (cdr n))) (car n)) alive)
           (setq count (+ count 1))
         )
       )
@@ -59,26 +56,26 @@
     )
   )
 
-;; Any live cell with two or three live neighbours survives.
-;; Any dead cell with three live neighbours becomes a live cell.
-;; All other live cells die in the next generation. Similarly, all other dead cells stay dead.
-
-(defun next-generation()
+(defun gol-next-generation()
+  (erase-buffer)
   (let ((new-board-array (make-vector size-of-board nil)))
     (dotimes (i size-of-board)
       (setf (aref new-board-array i) (make-vector size-of-board 0))
       (dotimes (j size-of-board)
         (let (
-              (count (neighbor-count j i))
-              (live (if (eq (elt (elt board-array i) j) (char-from-name "BLACK LARGE SQUARE")) t nil))
+              (count (gol-neighbor-count j i))
+              (live (if (eq (elt (elt board-array i) j) alive) t nil))
               )
+          ;; Any live cell with two or three live neighbours survives.
+          ;; Any dead cell with three live neighbours becomes a live cell.
+          ;; All other live cells die in the next generation. Similarly, all other dead cells stay dead.
           (cond
            ((and live (or (eq count 2) (eq count 3)))
-            (setf (elt (elt new-board-array i) j) (char-from-name "BLACK LARGE SQUARE")))
+            (setf (elt (elt new-board-array i) j) alive))
            ((and (not live) (eq count 3))
-            (setf (elt (elt new-board-array i) j) (char-from-name "BLACK LARGE SQUARE")))
+            (setf (elt (elt new-board-array i) j) alive))
            (t ;; default case
-            (setf (elt (elt new-board-array i) j) (char-from-name "WHITE LARGE SQUARE")))
+            (setf (elt (elt new-board-array i) j) dead))
            )
           )
         (insert (elt (elt new-board-array i) j))
@@ -89,16 +86,19 @@
     )
   )
 
-(defun init-gol ()
-  "Initialize GOL board"
+(defun gol-start ()
+  "Start a game of life."
   (interactive)
   (switch-to-buffer (get-buffer-create "gol"))
-  (initialize-board)
+  (gol-initialize-board)
+  (setq gol-timer (run-with-timer 0.5 0.5 `gol-next-generation))
   )
 
-(defun play-gol ()
-  "Play GOL"
-  ;; (interactive)
-  (erase-buffer)
-  (next-generation)
+(defun gol-stop ()
+  "Stop a game of life."
+  (interactive)
+  (cancel-timer gol-timer)
   )
+
+(provide `gol)
+;;; gol.el ends here
